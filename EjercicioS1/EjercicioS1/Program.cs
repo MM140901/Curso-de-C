0c14fbc2d996830992683658e7d46bddb14a20be
MM140901 <130320068+MM140901@users.noreply.github.com>
2023-07-13 13:01:25 -0600

    Add files via upload

---
+++ EjercicioS1/EjercicioS1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string nombre, hora, minutos;
+// Escribe un programa que reciba tu nombre y lo escriba por consola. //
+Console.WriteLine("Escriba su nombre");
+nombre = Console.ReadLine().ToString();
+
+Console.WriteLine("Su nombre es " + nombre);
+
+Console.WriteLine("Escriba la hora");
+hora = Console.ReadLine().ToString();
+Console.WriteLine("Escriba los minutos");
+minutos = Console.ReadLine().ToString();
+
+Console.WriteLine("La hora es " + hora + ":" + minutos);
+
+
+
+
